3ce0c8c5eca93e96407ad320ed04490edfdf5371
Sai Vanam <90760195+vanambharat@users.noreply.github.com>
2022-07-25 03:49:37 +0530

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      title: "TITANIC",
+      likes: Math.floor(Math.random() * (50 - 0) + 0),
+      image: "https://i.natgeofe.com/k/78854f3f-e5fb-4c76-ab91-e9a6ef379e6f/TitanicSailing-crop_16x9.jpg",
+    },
+    {
+      title: "SHAWSHANK REDEMPTION",
+      likes: Math.floor(Math.random() * (50 - 0) + 0),
+      image: "https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg",
+    },
+    {
+      title: "WORLD WAR Z",
+      likes: Math.floor(Math.random() * (50 - 0) + 0),
+      image: "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
+    },
+    {
+      title: "JOURNEY TO THE CENTER OF THE EARTH",
+      likes: Math.floor(Math.random() * (50 - 0) + 0),
+      image: "https://images.moviesanywhere.com/ae014daea22630be314e45fd0be2dbac/14788d11-e279-4963-bb0a-5161c5b391cf.jpg",
+    },
+  ];
